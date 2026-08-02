93ae04e566145c406a75ddbd14e17e8cf026ec75
liu-suria <liu@shual.cn>
2026-08-02 17:17:52 +0800

    Bump version for revision sync fix

--- app-config.js
+++ app-config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = Object.freeze({
   name: 'Family Hub',
-  version: 'Beta v2.9.3 RC',
+  version: 'Beta v2.9.4 RC',
   dataVersion: 8,
-  cacheRevision: '293',
+  cacheRevision: '294',
   timeZone: 'Asia/Shanghai'
 });
