9d716300774f5b8feba4e45fbf747a23f97f0116
liu-suria <liu@shual.cn>
2026-08-02 18:40:57 +0800

    Bump version for swipe details

--- app-config.js
+++ app-config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = Object.freeze({
   name: 'Family Hub',
-  version: 'Beta v2.9.6 RC',
+  version: 'Beta v2.9.7 RC',
   dataVersion: 8,
-  cacheRevision: '296',
+  cacheRevision: '297',
   timeZone: 'Asia/Shanghai'
 });
