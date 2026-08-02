4ca8da0b2a6f8b1e1a1d291f7ffbe81a3ff403e3
liu-suria <liu@shual.cn>
2026-08-02 17:57:19 +0800

    Bump fast-save release version

--- app-config.js
+++ app-config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = Object.freeze({
   name: 'Family Hub',
-  version: 'Beta v2.9.5 RC',
+  version: 'Beta v2.9.6 RC',
   dataVersion: 8,
-  cacheRevision: '295',
+  cacheRevision: '296',
   timeZone: 'Asia/Shanghai'
 });
