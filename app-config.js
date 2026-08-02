4c21286bc794d6491c088a6e3d20bc01de2c2e5c
liu-suria <liu@shual.cn>
2026-08-02 16:40:20 +0800

    Bump performance cleanup release

--- app-config.js
+++ app-config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = Object.freeze({
   name: 'Family Hub',
-  version: 'Beta v2.9.1 RC',
+  version: 'Beta v2.9.2 RC',
   dataVersion: 8,
-  cacheRevision: '291',
+  cacheRevision: '292',
   timeZone: 'Asia/Shanghai'
 });
