7f2041a8240252d681d3b0329f6a076f1985ddee
liu-suria <liu@shual.cn>
2026-08-02 16:54:20 +0800

    Bump final polish release

--- app-config.js
+++ app-config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = Object.freeze({
   name: 'Family Hub',
-  version: 'Beta v2.9.2 RC',
+  version: 'Beta v2.9.3 RC',
   dataVersion: 8,
-  cacheRevision: '292',
+  cacheRevision: '293',
   timeZone: 'Asia/Shanghai'
 });
